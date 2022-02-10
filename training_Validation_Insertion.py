87c6d0d2c6367a4aa39bc0d69d9cff15250641a6
ThakurSneha <thakursneha578@gmail.com>
2022-02-10 21:02:29 +0530

    final commit

--- training_Validation_Insertion.py
+++ training_Validation_Insertion.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 from Training_Raw_data_validation.rawValidation import Raw_Data_validation
-# from DataTypeValidation_Insertion_Training.DataTypeValidation import dBOperation
 from DataTransform_Training.DataTransformation import dataTransform
 from application_logging import logger
 
